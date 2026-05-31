b5dd2056a3e7d06591fc6369fb11a026df43542c
Usuario <Usuario@LAPTOP-ID4HLSDH>
2026-05-31 08:14:02 -0600

    Agrega excepcion personalizada

---
+++ SistemaDeGestionAcademica/src/DatoInvalidoExcep.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class DatoInvalidoExcep extends Exception{
+    public DatoInvalidoExcep(String mensaje) {
+        super(mensaje);
+    }
+}

307c4b9caecbb9b4217050d4c299dad517cc3507
yarie <yarie@192.168.1.61>
2026-05-31 10:22:06 -0600

    Herencia de clases

---
+++ SistemaDeGestionAcademica/src/sistemadegestionacademica/Estudiante.java
@@ -0,0 +1,31 @@
+package sistemadegestionacademica;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author yarie
+ */
+
+// Creación de la clase estudiante que hereda de persona
+public class Estudiante extends Persona{
+    public String carrera; // Atributo carrera
+    
+    //Constructor que utiliza super
+    public Estudiante(String nombre, String id, Direccion direccion, String carrera) {
+        super(nombre, id, direccion);
+        this.carrera = carrera;
+    }
+
+    //Setter y getter
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
